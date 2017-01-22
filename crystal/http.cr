9c9b694184f70e4b903e40ee8fadb9f7c4ab1915
Virtual-Machine <darrelm@tech-asteroid.com>
2017-01-22 15:47:02 -0500

    Add a http example for crystal, must have normal prelude

---
+++ crystal/http.cr
@@ -0,0 +1,9 @@
+require "http/server"
+
+server = HTTP::Server.new(8080) do |context|
+  context.response.content_type = "text/plain"
+  context.response.print "Hello world! The time is #{Time.now}"
+end
+
+puts "Listening on http://127.0.0.1:8080"
+server.listen
